460f0ded0375b2eff2cfe469e5f89902088f67e1
chendonghui <chendonghui@zhongdihang.com>
2022-04-08 16:58:23 +0800

    基本语法、绘制图形

---
+++ src/matlab/demo1.m
@@ -0,0 +1,7 @@
+a = 100;
+if a < 20
+    fprintf('a is less than 20\n');
+else
+    fprintf('a is not less than 20\n');
+end
+fprintf('value of a is:%d\n', a);
---
+++ src/matlab/demo3.m
@@ -0,0 +1,11 @@
+for a = 10 : 2 : 20
+    fprintf('value of a is: %d\n', a);
+end
+fprintf('---------------------------------\n')
+for a = 1.0 : -0.1 : 0.0
+    disp(a)
+end
+fprintf('---------------------------------\n')
+for a = [23, 33, 43, 53, 63]
+    disp(a)
+end
